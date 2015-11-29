44ac3c19560a8e2c2cba47834af7a72b32f52053
Daniel Gibbs <daniel@danielgibbs.name>
2015-11-30 10:20:14 +1300

    - Performance improvements

--- NSBundle+OBCodeSigningInfo.m
+++ NSBundle+OBCodeSigningInfo.m
@@ -18,8 +18,7 @@
 
 @implementation NSBundle (OBCodeSigningInfo)
 
-- (BOOL)ob_comesFromAppStore
-{
+- (BOOL)ob_comesFromAppStore {
     // Check existence of Mac App Store receipt
     NSURL *appStoreReceiptURL = [self appStoreReceiptURL];
     NSFileManager *fileManager = [[NSFileManager alloc] init];
@@ -27,84 +26,69 @@
     return appStoreReceiptExists;
 }
 
-
-- (BOOL)ob_isSandboxed
-{
-    BOOL isSandboxed = NO;
-    if ([self ob_codeSignState] == OBCodeSignStateSignatureValid) 
-    {
-        SecStaticCodeRef staticCode = [self ob_createStaticCode];
-        SecRequirementRef sandboxRequirement = [self ob_sandboxRequirement];
-        if (staticCode && sandboxRequirement) {
-            OSStatus codeCheckResult = SecStaticCodeCheckValidityWithErrors(staticCode, kSecCSBasicValidateOnly, sandboxRequirement, NULL);
-            if (codeCheckResult == errSecSuccess) {
-                isSandboxed = YES;
+- (BOOL)ob_isSandboxed {
+    static BOOL isSandboxed = NO;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        if ([self ob_codeSignState] == OBCodeSignStateSignatureValid) {
+            SecStaticCodeRef staticCode = [self ob_createStaticCode];
+            SecRequirementRef sandboxRequirement = [self ob_sandboxRequirement];
+            if (staticCode && sandboxRequirement) {
+                OSStatus codeCheckResult = SecStaticCodeCheckValidityWithErrors(staticCode, kSecCSBasicValidateOnly, sandboxRequirement, NULL);
+                if (codeCheckResult == errSecSuccess) {
+                    isSandboxed = YES;
+                }
+                CFRelease(staticCode);
             }
-            CFRelease(staticCode);
         }
-    }
+    });
     return isSandboxed;
 }
 
-
-- (OBCodeSignState)ob_codeSignState
-{
-    // Return cached value if it exists
-    static const void *kOBCodeSignStateKey;
-    NSNumber *resultStateNumber = objc_getAssociatedObject(self, kOBCodeSignStateKey);
-    if (resultStateNumber) {
-        return (OBCodeSignState) [resultStateNumber intValue];
-    }
-
-    // Determine code sign status
-    OBCodeSignState resultState = OBCodeSignStateError;
-    SecStaticCodeRef staticCode = [self ob_createStaticCode];
-    if (staticCode) 
-    {
-        OSStatus signatureCheckResult = SecStaticCodeCheckValidityWithErrors(staticCode, kSecCSBasicValidateOnly, NULL, NULL);
-        switch (signatureCheckResult) {
-            case errSecSuccess: resultState = OBCodeSignStateSignatureValid; break;
-            case errSecCSUnsigned: resultState = OBCodeSignStateUnsigned; break;
-            case errSecCSSignatureFailed:
-            case errSecCSSignatureInvalid:
-                resultState = OBCodeSignStateSignatureInvalid;
-                break;
-            case errSecCSSignatureNotVerifiable: resultState = OBCodeSignStateSignatureNotVerifiable; break;
-            case errSecCSSignatureUnsupported: resultState = OBCodeSignStateSignatureUnsupported; break;
-            default: resultState = OBCodeSignStateError; break;
+- (OBCodeSignState)ob_codeSignState {
+    static OBCodeSignState resultState = OBCodeSignStateError;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        // Determine code sign status
+        SecStaticCodeRef staticCode = [self ob_createStaticCode];
+        if (staticCode) {
+            OSStatus signatureCheckResult = SecStaticCodeCheckValidityWithErrors(staticCode, kSecCSBasicValidateOnly, NULL, NULL);
+            switch (signatureCheckResult) {
+                case errSecSuccess: resultState = OBCodeSignStateSignatureValid; break;
+                case errSecCSUnsigned: resultState = OBCodeSignStateUnsigned; break;
+                case errSecCSSignatureFailed:
+                case errSecCSSignatureInvalid:
+                    resultState = OBCodeSignStateSignatureInvalid;
+                    break;
+                case errSecCSSignatureNotVerifiable: resultState = OBCodeSignStateSignatureNotVerifiable; break;
+                case errSecCSSignatureUnsupported: resultState = OBCodeSignStateSignatureUnsupported; break;
+                default: resultState = OBCodeSignStateError; break;
+            }
+            CFRelease(staticCode);
         }
-        CFRelease(staticCode);
-    }
-    else
-    {
-        resultState = OBCodeSignStateError;
-    }
-    
-    // Cache the result
-    resultStateNumber = [NSNumber numberWithInteger:resultState];
-    objc_setAssociatedObject(self, kOBCodeSignStateKey, resultStateNumber, OBJC_ASSOCIATION_RETAIN);
-    
+        else {
+            resultState = OBCodeSignStateError;
+        }
+    });
     return resultState;
 }
 
-
 #pragma mark - Private helper methods
 
-- (SecStaticCodeRef)ob_createStaticCode
-{
+// These methods don't use dispatch_once for memory management reasons: if another method called these more than once
+// then it might try to free the result twice which would be bad. Instead we just use dispatch_once in the methods that
+// call these ones.
+
+- (SecStaticCodeRef)ob_createStaticCode {
     NSURL *bundleURL = [self bundleURL];
     SecStaticCodeRef staticCode = NULL;
     SecStaticCodeCreateWithPath((__bridge CFURLRef)bundleURL, kSecCSDefaultFlags, &staticCode);
     return staticCode;
 }
 
-- (SecRequirementRef)ob_sandboxRequirement
-{
-    static SecRequirementRef sandboxRequirement = NULL;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        SecRequirementCreateWithString(CFSTR("entitlement[\"com.apple.security.app-sandbox\"] exists"), kSecCSDefaultFlags, &sandboxRequirement);
-    });
+- (SecRequirementRef)ob_sandboxRequirement {
+    SecRequirementRef sandboxRequirement = NULL;
+    SecRequirementCreateWithString(CFSTR("entitlement[\"com.apple.security.app-sandbox\"] exists"), kSecCSDefaultFlags, &sandboxRequirement);
     return sandboxRequirement;
 }
 
